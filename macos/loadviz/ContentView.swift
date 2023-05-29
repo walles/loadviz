b7a26fd347fdae895efc919d31ff51f8999339ec
Johan Walles <johan.walles@gmail.com>
2023-05-29 06:53:56 +0200

    Add initial macOS code

---
+++ macos/loadviz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  loadviz
+//
+//  Created by Johan Walles on 2023-05-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ macos/loadviz/loadvizApp.swift
@@ -0,0 +1,17 @@
+//
+//  loadvizApp.swift
+//  loadviz
+//
+//  Created by Johan Walles on 2023-05-29.
+//
+
+import SwiftUI
+
+@main
+struct loadvizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
