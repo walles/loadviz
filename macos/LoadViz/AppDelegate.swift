5e7b76fa79c9aeb5c9d16b4be320c28e4c72883c
Johan Walles <johan.walles@gmail.com>
2023-06-13 07:06:19 +0200

    Add working menus

--- macos/LoadViz/AppDelegate.swift
+++ macos/LoadViz/AppDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  AppDelegate.swift
-//  LoadViz
-//
-//  Created by Johan Walles on 2023-06-13.
-//
-
 import Cocoa
 
 class AppDelegate: NSObject, NSApplicationDelegate {
@@ -15,5 +8,44 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     if let button = statusItem.button {
       button.image = NSImage(systemSymbolName: "1.circle", accessibilityDescription: "1")
     }
+
+    setupMenus()
+  }
+
+  private func setupMenus() {
+    let menu = NSMenu()
+
+    let one = NSMenuItem(title: "One", action: #selector(didTapOne), keyEquivalent: "1")
+    menu.addItem(one)
+
+    let two = NSMenuItem(title: "Two", action: #selector(didTapTwo), keyEquivalent: "2")
+    menu.addItem(two)
+
+    let three = NSMenuItem(title: "Three", action: #selector(didTapThree), keyEquivalent: "3")
+    menu.addItem(three)
+
+    menu.addItem(NSMenuItem.separator())
+
+    menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+
+    statusItem.menu = menu
+  }
+
+  private func changeStatusBarButton(number: Int) {
+    if let button = statusItem.button {
+      button.image = NSImage(systemSymbolName: "\(number).circle", accessibilityDescription: number.description)
+    }
+  }
+
+  @objc func didTapOne() {
+    changeStatusBarButton(number: 1)
+  }
+
+  @objc func didTapTwo() {
+    changeStatusBarButton(number: 2)
+  }
+
+  @objc func didTapThree() {
+    changeStatusBarButton(number: 3)
   }
 }
