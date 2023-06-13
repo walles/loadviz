fa6c516eac43f0c58b0e8da77b053dd407470414
Johan Walles <johan.walles@gmail.com>
2023-06-13 06:52:58 +0200

    Create a storyboard app

---
+++ macos/LoadViz/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LoadViz
+//
+//  Created by Johan Walles on 2023-06-13.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
