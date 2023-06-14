211547764c961149ce1131dc8b145c59813285da
Johan Walles <johan.walles@gmail.com>
2023-06-14 07:00:23 +0200

    Add a Quit menu item

--- macos/LoadViz/AppDelegate.swift
+++ macos/LoadViz/AppDelegate.swift
@@ -1,51 +1,67 @@
 import Cocoa
 
+// From: https://stackoverflow.com/a/38596649/473672
+func imageFromPixels(pixels: UnsafePointer<UInt8>, width: Int, height: Int) -> NSImage {
+  let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+
+  // FIXME: Verify this gets us the correct colors
+  let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.none.rawValue)
+
+  let bitsPerComponent = 8 // number of bits in UInt8
+  let bitsPerPixel = 3 * bitsPerComponent // RGB uses 3 components
+  let bytesPerRow = bitsPerPixel * width / 8
+  let providerRef = CGDataProvider(
+    data: NSData(bytes: pixels, length: height * bytesPerRow)
+  )
+
+  let cgim = CGImage(
+    width: width,
+    height: height,
+    bitsPerComponent: bitsPerComponent,
+    bitsPerPixel: bitsPerPixel,
+    bytesPerRow: bytesPerRow,
+    space: rgbColorSpace,
+    bitmapInfo: bitmapInfo,
+    provider: providerRef!,
+    decode: nil,
+    shouldInterpolate: true,
+    intent: .defaultIntent
+  )
+
+  return NSImage(cgImage: cgim!, size: NSSize(width: width, height: height))
+}
+
 class AppDelegate: NSObject, NSApplicationDelegate {
   private var statusItem: NSStatusItem!
+  private var libLoadViz = LibLoadViz.new_loadviz()
 
   func applicationDidFinishLaunching(_ aNotification: Notification) {
     statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
     if let button = statusItem.button {
-      button.image = NSImage(systemSymbolName: "1.circle", accessibilityDescription: "1")
+      Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [self] _ in
+        button.image = getNewButtonImage()
+      }
+      button.image = getNewButtonImage()
     }
 
     setupMenus()
   }
 
+  private func getNewButtonImage() -> NSImage {
+    let width = 40
+    let height = 22
+    return imageFromPixels(
+      pixels: LibLoadViz.get_image(libLoadViz, UInt(width), UInt(height)),
+      width: width,
+      height: height
+    )
+  }
+
   private func setupMenus() {
     let menu = NSMenu()
-
-    let one = NSMenuItem(title: "One", action: #selector(didTapOne), keyEquivalent: "1")
-    menu.addItem(one)
-
-    let two = NSMenuItem(title: "Two", action: #selector(didTapTwo), keyEquivalent: "2")
-    menu.addItem(two)
-
-    let three = NSMenuItem(title: "Three", action: #selector(didTapThree), keyEquivalent: "3")
-    menu.addItem(three)
-
-    menu.addItem(NSMenuItem.separator())
 
     menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
 
     statusItem.menu = menu
-  }
-
-  private func changeStatusBarButton(number: Int) {
-    if let button = statusItem.button {
-      button.image = NSImage(systemSymbolName: "\(number).circle", accessibilityDescription: number.description)
-    }
-  }
-
-  @objc func didTapOne() {
-    changeStatusBarButton(number: 1)
-  }
-
-  @objc func didTapTwo() {
-    changeStatusBarButton(number: 2)
-  }
-
-  @objc func didTapThree() {
-    changeStatusBarButton(number: 3)
   }
 }
