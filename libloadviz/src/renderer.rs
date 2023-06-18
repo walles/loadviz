b462d973b2c108705f792b0841436f0d0e067bcc
Johan Walles <johan.walles@gmail.com>
2023-06-18 21:51:47 +0200

    Pass a time dimension into the Perlin function

--- libloadviz/src/renderer.rs
+++ libloadviz/src/renderer.rs
@@ -1,6 +1,8 @@
+use std::time::Instant;
+
 use noise::{NoiseFn, Perlin};
 
-use crate::{cpuload::CpuLoad, LoadViz};
+use crate::cpuload::CpuLoad;
 
 static BG_COLOR_RGB: &[u8; 3] = &[0x30, 0x30, 0x90];
 static BG_COLOR_RGB_DARK: &[u8; 3] = &[0x18, 0x18, 0x48];
@@ -9,66 +11,66 @@ static USER_LOAD_COLOR_RGB_DARK: &[u8; 3] = &[0x00, 0x80, 0x00];
 static SYSTEM_LOAD_COLOR_RGB: &[u8; 3] = &[0xff, 0x00, 0x00];
 static SYSTEM_LOAD_COLOR_RGB_DARK: &[u8; 3] = &[0x80, 0x00, 0x00];
 
-impl LoadViz {
-    pub(crate) fn render_image(&mut self) {
-        if self.load_reader.get_loads().is_empty() {
-            // FIXME: Draw something nice?
-            return;
+pub struct Renderer {
+    perlin: Perlin,
+    t0: Instant,
+}
+
+impl Renderer {
+    pub fn new() -> Self {
+        Self {
+            perlin: Perlin::new(Perlin::DEFAULT_SEED),
+            t0: Instant::now(),
         }
+    }
 
-        self.update_currently_displayed_loads();
+    /// Don't call this! It's public for benchmarking purposes only.
+    ///
+    /// You should call `LoadViz::render_image()` instead.
+    pub fn render_image(
+        &self,
+        currently_displayed_loads: &Vec<CpuLoad>,
+        width: usize,
+        height: usize,
+        pixels: &mut Vec<u8>,
+    ) {
+        let viz_loads = mirror_sort(currently_displayed_loads);
 
-        render_image_raw(
-            &self.currently_displayed_loads,
-            self.width,
-            self.height,
-            &mut self.pixels,
-        );
+        for i in (0..pixels.len()).step_by(3) {
+            let x = (i / 3) % width;
+            let y = height - (i / 3) / width - 1;
+
+            // Higher scale number = more details.
+            let scale = 20.0 / width as f64;
+            let time: f64 = self.t0.elapsed().as_secs_f64();
+
+            // NOTE: Experiments show that the Perlin output is -1 to 1
+            let number = self.perlin.get([scale * x as f64, scale * y as f64, time]);
+            let number = (number + 1.0) / 2.0; // Normalize into 0 to 1
+
+            let cpu_load = &viz_loads[(x * viz_loads.len()) / width];
+
+            let y_height = y as f32 / height as f32;
+            let idle_0_to_1 = 1.0 - (cpu_load.user_0_to_1 + cpu_load.system_0_to_1);
+            let user_plus_idle_height = cpu_load.user_0_to_1 + idle_0_to_1;
+            let color = if y_height > user_plus_idle_height {
+                interpolate(number, SYSTEM_LOAD_COLOR_RGB_DARK, SYSTEM_LOAD_COLOR_RGB)
+            } else if y_height > cpu_load.user_0_to_1 {
+                interpolate(number, BG_COLOR_RGB_DARK, BG_COLOR_RGB)
+            } else {
+                interpolate(number, USER_LOAD_COLOR_RGB_DARK, USER_LOAD_COLOR_RGB)
+            };
+
+            pixels[i] = color[0];
+            pixels[i + 1] = color[1];
+            pixels[i + 2] = color[2];
+        }
     }
 }
 
-/// Don't call this! It's public for benchmarking purposes only.
-///
-/// You should call `LoadViz::render_image()` instead.
-pub fn render_image_raw(
-    currently_displayed_loads: &Vec<CpuLoad>,
-    width: usize,
-    height: usize,
-    pixels: &mut Vec<u8>,
-) {
-    let viz_loads = mirror_sort(currently_displayed_loads);
-
-    // FIXME: Reuse the same Perlin instance for all calls to this function.
-    let perlin = Perlin::new(Perlin::DEFAULT_SEED);
-
-    for i in (0..pixels.len()).step_by(3) {
-        let x = (i / 3) % width;
-        let y = height - (i / 3) / width - 1;
-
-        // FIXME: Pass a time dimension to the noise function. Higher number =
-        // more details.
-        let scale = 20.0 / width as f64;
-
-        // NOTE: Experiments show that the output here is -1 to 1
-        let number = perlin.get([scale * x as f64, scale * y as f64]);
-        let number = (number + 1.0) / 2.0; // Normalize into 0 to 1
-
-        let cpu_load = &viz_loads[(x * viz_loads.len()) / width];
-
-        let y_height = y as f32 / height as f32;
-        let idle_0_to_1 = 1.0 - (cpu_load.user_0_to_1 + cpu_load.system_0_to_1);
-        let user_plus_idle_height = cpu_load.user_0_to_1 + idle_0_to_1;
-        let color = if y_height > user_plus_idle_height {
-            interpolate(number, SYSTEM_LOAD_COLOR_RGB_DARK, SYSTEM_LOAD_COLOR_RGB)
-        } else if y_height > cpu_load.user_0_to_1 {
-            interpolate(number, BG_COLOR_RGB_DARK, BG_COLOR_RGB)
-        } else {
-            interpolate(number, USER_LOAD_COLOR_RGB_DARK, USER_LOAD_COLOR_RGB)
-        };
-
-        pixels[i] = color[0];
-        pixels[i + 1] = color[1];
-        pixels[i + 2] = color[2];
+impl Default for Renderer {
+    fn default() -> Self {
+        Self::new()
     }
 }
 
@@ -101,8 +103,6 @@ fn interpolate(factor_0_to_1: f64, color1: &[u8; 3], color2: &[u8; 3]) -> [u8; 3
 
 #[cfg(test)]
 mod tests {
-    use std::time::Instant;
-
     use crate::cpuload::CpuLoad;
 
     use super::mirror_sort;
@@ -122,19 +122,10 @@ mod tests {
     fn test_render_nothing() {
         let width = 10;
         let height = 10;
-        let pixels = vec![0; width * height * 3];
+        let mut pixels = vec![0; width * height * 3];
+        let renderer = super::Renderer::new();
 
-        let mut loadviz = super::LoadViz {
-            width,
-            height,
-            pixels,
-            currently_displayed_loads: Vec::new(),
-            currently_displayed_loads_updated: Instant::now(),
-
-            // Create a load reader that always says there are no cores
-            load_reader: crate::load_reader::LoadReader::new(std::vec::Vec::new),
-        };
-        loadviz.render_image();
+        renderer.render_image(&Vec::new(), width, height, &mut pixels);
     }
 
     #[test]
