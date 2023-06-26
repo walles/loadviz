22879e53f5d6864b5a3770cfdac75792f7204cec
Johan Walles <johan.walles@gmail.com>
2023-06-26 10:41:21 +0200

    screenshot.rs skeleton

---
+++ libloadviz/src/bin/screenshot.rs
@@ -0,0 +1,11 @@
+/// Writes a screenshot to a file
+
+fn main() {
+    // FIXME: Put this file in the same directory as Cargo.toml
+    let filename = "screenshot.webp";
+
+    println!(
+        "Imagine this program writing a screenshot into {}",
+        filename
+    );
+}
