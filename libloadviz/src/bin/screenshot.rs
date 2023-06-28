9e82365e9eef42a93d14b0cb0a88b5c427d082f8
Johan Walles <johan.walles@gmail.com>
2023-06-28 06:45:07 +0200

    WIP Render an animated screenshot
    But it must loop! It doesn't.

--- libloadviz/src/bin/screenshot.rs
+++ libloadviz/src/bin/screenshot.rs
@@ -1,10 +1,17 @@
+#![allow(clippy::needless_return)]
+
 /// Writes a screenshot to a file
 use std::fs;
 
 use libloadviz::renderer::Renderer;
-use libwebp_sys::WebPEncodeRGB;
+use webp_animation::prelude::*;
 
 fn main() {
+    let width = 100;
+    let height = 75;
+    let frames_per_second = 10;
+    let seconds = 10;
+
     // FIXME: Put this file in the same directory as Cargo.toml
     let filename = "screenshot.webp";
 
@@ -23,27 +30,50 @@ fn main() {
         },
     ];
 
-    let width = 100;
-    let height = 75;
     let mut pixels = vec![0u8; width * height * 3];
     let renderer: Renderer = Default::default();
-    renderer.render_image(&loads, width, height, 0.0, &mut pixels);
 
-    // Ref: https://crates.io/crates/libwebp-sys
-    let encoded: Vec<u8>;
-    unsafe {
-        let mut out_buf = std::ptr::null_mut();
-        let stride = width as i32 * 3;
-        let encoded_length = WebPEncodeRGB(
-            pixels.as_ptr(),
-            width as i32,
-            height as i32,
-            stride,
-            50.0,
-            &mut out_buf,
-        );
-        encoded = std::slice::from_raw_parts(out_buf, encoded_length).into();
+    // FIXME: Add something about looping the animation
+    let mut encoder = Encoder::new_with_options(
+        (width as u32, height as u32),
+        EncoderOptions {
+            minimize_size: false, // FIXME: True?
+            kmin: 0,
+            kmax: 0, // FIXME: Up this to get keyframes
+            allow_mixed: true,
+            verbose: true,
+            color_mode: ColorMode::Rgba,
+            encoding_config: Some(EncodingConfig {
+                encoding_type: EncodingType::Lossy(Default::default()),
+                quality: 80.0,
+                method: 6,
+            }),
+        },
+    )
+    .unwrap();
+
+    for i in 0..(frames_per_second * seconds - 1) {
+        let dt_seconds = i as f64 / frames_per_second as f64;
+
+        renderer.render_image(&loads, width, height, dt_seconds, &mut pixels);
+
+        let dt_milliseconds = (dt_seconds * 1000.0) as i32;
+        encoder
+            .add_frame(&rgb_to_rgba(&pixels), dt_milliseconds)
+            .unwrap();
     }
 
-    fs::write(filename, encoded).expect("Unable to write file");
+    let final_milliseconds = frames_per_second * seconds * 1000;
+    let webp_data = encoder.finalize(final_milliseconds).unwrap();
+    fs::write(filename, webp_data).expect("Unable to write file");
+}
+
+// Workaround for: https://github.com/blaind/webp-animation/issues/11
+fn rgb_to_rgba(pixels: &[u8]) -> Vec<u8> {
+    let mut rgba = Vec::with_capacity(pixels.len() * 4 / 3);
+    for pixel in pixels.chunks_exact(3) {
+        rgba.extend_from_slice(pixel);
+        rgba.push(255);
+    }
+    return rgba;
 }
