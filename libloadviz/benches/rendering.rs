1004e451e92805e34a22571a16c38a9f18d5e5cd
Johan Walles <johan.walles@gmail.com>
2023-06-30 07:05:59 +0200

    Add an idle-system benchmark
    Since that's where most desktop systems usually are. We should be able
    to do better in this case.

--- libloadviz/benches/rendering.rs
+++ libloadviz/benches/rendering.rs
@@ -1,7 +1,7 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use libloadviz::renderer::Renderer;
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn rendering_busy(c: &mut Criterion) {
     let width = 100;
     let height = 100;
     let mut pixels = vec![0; width * height * 3];
@@ -23,5 +23,27 @@ fn criterion_benchmark(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn rendering_idle(c: &mut Criterion) {
+    let width = 100;
+    let height = 100;
+    let mut pixels = vec![0; width * height * 3];
+    let renderer: Renderer = Default::default();
+
+    let cpu_loads = vec![
+        libloadviz::cpuload::CpuLoad {
+            user_0_to_1: 0.05,
+            system_0_to_1: 0.05,
+        },
+        libloadviz::cpuload::CpuLoad {
+            user_0_to_1: 0.05,
+            system_0_to_1: 0.05,
+        },
+    ];
+
+    c.bench_function("render 100x100 image on an idle system", |b| {
+        b.iter(|| renderer.render_image(black_box(&cpu_loads), width, height, 0.0, &mut pixels));
+    });
+}
+
+criterion_group!(benches, rendering_busy, rendering_idle);
 criterion_main!(benches);
