3c0fab73a7b8acee905265692a4d0f050358a3fc
Johan Walles <johan.walles@gmail.com>
2023-07-09 08:08:32 +0200

    Move cloud code to its own file

--- libloadviz/src/renderer.rs
+++ libloadviz/src/renderer.rs
@@ -8,17 +8,6 @@ static BG_COLOR_RGB: &[u8; 3] = &[0x30, 0x30, 0x90];
 static USER_LOAD_COLOR_RGB_WARMER: &[u8; 3] = &[0xff, 0xb4, 0x6b]; // 3000K
 static USER_LOAD_COLOR_RGB_COOLER: &[u8; 3] = &[0xff, 0x38, 0x00]; // 1000K
 
-static CLOUD_COLOR_DARK: &[u8; 3] = &[0x88, 0x88, 0x88];
-static CLOUD_COLOR_BRIGHT: &[u8; 3] = &[0xff, 0xff, 0xff];
-
-/// How much of the cloud should fade towards transparent?
-///
-/// This is a fraction of the height of the whole image, not a fraction of the
-/// height of the cloud.
-///
-/// Lower values make the cloud edge sharper.
-static CLOUD_TRANSPARENT_FRACTION: f32 = 0.4;
-
 pub struct Renderer {
     noise: FastNoise,
 }
@@ -30,6 +19,8 @@ impl Default for Renderer {
         };
     }
 }
+
+mod cloud;
 
 impl Renderer {
     /// Don't call this! It's public for benchmarking purposes only.
@@ -81,61 +72,6 @@ impl Renderer {
                 pixels[i + 2] = color[2];
             }
         }
-    }
-
-    fn get_cloud_pixel(
-        &self,
-        viz_loads: &Vec<CpuLoad>,
-        dt_seconds: f32,
-        pixel_x: usize,
-        pixel_y_from_top: usize,
-        width: usize,
-        height: usize,
-    ) -> Option<[u8; 3]> {
-        // Higher number = more details.
-        let detail = 5.0 / width as f32;
-
-        // Higher speed number = faster cloud turbulence.
-        let speed = 0.3;
-
-        let x_fraction_0_to_1 = pixel_x as f32 / (width as f32 - 1.0);
-        let cpu_load = get_load(viz_loads, x_fraction_0_to_1);
-
-        // Compute the sysload height for this load
-        let cloud_height_pixels = cpu_load.system_0_to_1 * height as f32;
-        if pixel_y_from_top as f32 > cloud_height_pixels {
-            return None;
-        }
-
-        // Noise output is -1 to 1, deciphered from here:
-        // https://github.com/amethyst/bracket-lib/blob/0d2d5e6a9a8e7c7ae3710cfef85be4cab0109a27/bracket-noise/examples/simplex_fractal.rs#L34-L39
-        let noise_m1_to_1 = self.noise.get_noise3d(
-            detail * pixel_x as f32,
-            detail * pixel_y_from_top as f32,
-            speed * dt_seconds,
-        );
-
-        let brightness_0_to_1 = (noise_m1_to_1 + 1.0) / 2.0;
-        let color = interpolate(brightness_0_to_1, CLOUD_COLOR_DARK, CLOUD_COLOR_BRIGHT);
-
-        let transparency_height_pixels = CLOUD_TRANSPARENT_FRACTION * height as f32;
-        let opaque_height_pixels = cloud_height_pixels - transparency_height_pixels;
-        if (pixel_y_from_top as f32) < opaque_height_pixels {
-            // Cloud interior
-            return Some(color);
-        }
-
-        // When we get here, we're closer to the edge of the cloud
-
-        // 0-1, higher means more transparent
-        let alpha = (pixel_y_from_top as f32 - opaque_height_pixels) / transparency_height_pixels;
-
-        // Replace dark with transparent. Towards the edge of the cloud, we won't
-        // see as many dark colors since the sun won't be blocked by thick cloud
-        // parts.
-        let color = interpolate(alpha * (1.0 - brightness_0_to_1), &color, BG_COLOR_RGB);
-
-        return Some(interpolate(alpha, &color, BG_COLOR_RGB));
     }
 
     fn get_flame_pixel(
---
+++ libloadviz/src/renderer/cloud.rs
@@ -0,0 +1,71 @@
+use crate::cpuload::CpuLoad;
+
+use super::{get_load, interpolate, Renderer, BG_COLOR_RGB};
+
+static CLOUD_COLOR_DARK: &[u8; 3] = &[0x88, 0x88, 0x88];
+static CLOUD_COLOR_BRIGHT: &[u8; 3] = &[0xff, 0xff, 0xff];
+
+/// How much of the cloud should fade towards transparent?
+///
+/// This is a fraction of the height of the whole image, not a fraction of the
+/// height of the cloud.
+///
+/// Lower values make the cloud edge sharper.
+static CLOUD_TRANSPARENT_FRACTION: f32 = 0.4;
+
+impl Renderer {
+    pub(super) fn get_cloud_pixel(
+        &self,
+        viz_loads: &Vec<CpuLoad>,
+        dt_seconds: f32,
+        pixel_x: usize,
+        pixel_y_from_top: usize,
+        width: usize,
+        height: usize,
+    ) -> Option<[u8; 3]> {
+        // Higher number = more details.
+        let detail = 5.0 / width as f32;
+
+        // Higher speed number = faster cloud turbulence.
+        let speed = 0.3;
+
+        let x_fraction_0_to_1 = pixel_x as f32 / (width as f32 - 1.0);
+        let cpu_load = get_load(viz_loads, x_fraction_0_to_1);
+
+        // Compute the sysload height for this load
+        let cloud_height_pixels = cpu_load.system_0_to_1 * height as f32;
+        if pixel_y_from_top as f32 > cloud_height_pixels {
+            return None;
+        }
+
+        // Noise output is -1 to 1, deciphered from here:
+        // https://github.com/amethyst/bracket-lib/blob/0d2d5e6a9a8e7c7ae3710cfef85be4cab0109a27/bracket-noise/examples/simplex_fractal.rs#L34-L39
+        let noise_m1_to_1 = self.noise.get_noise3d(
+            detail * pixel_x as f32,
+            detail * pixel_y_from_top as f32,
+            speed * dt_seconds,
+        );
+
+        let brightness_0_to_1 = (noise_m1_to_1 + 1.0) / 2.0;
+        let color = interpolate(brightness_0_to_1, CLOUD_COLOR_DARK, CLOUD_COLOR_BRIGHT);
+
+        let transparency_height_pixels = CLOUD_TRANSPARENT_FRACTION * height as f32;
+        let opaque_height_pixels = cloud_height_pixels - transparency_height_pixels;
+        if (pixel_y_from_top as f32) < opaque_height_pixels {
+            // Cloud interior
+            return Some(color);
+        }
+
+        // When we get here, we're closer to the edge of the cloud
+
+        // 0-1, higher means more transparent
+        let alpha = (pixel_y_from_top as f32 - opaque_height_pixels) / transparency_height_pixels;
+
+        // Replace dark with transparent. Towards the edge of the cloud, we won't
+        // see as many dark colors since the sun won't be blocked by thick cloud
+        // parts.
+        let color = interpolate(alpha * (1.0 - brightness_0_to_1), &color, BG_COLOR_RGB);
+
+        return Some(interpolate(alpha, &color, BG_COLOR_RGB));
+    }
+}
